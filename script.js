38fbf2ee2355494a3bf57be2ff675eae4177a77d
auliaputriraeni-prog <auliaputriraeni@gmail.com>
2026-01-05 21:25:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showSection(id) {
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    document.getElementById(id).classList.add('active');
+}
+function showSection(id) {
+    const sections = document.querySelectorAll(".content");
+
+    sections.forEach(section => {
+        section.classList.remove("active");
+    });
+
+    document.getElementById(id).classList.add("active");
+}
